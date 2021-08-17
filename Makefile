6126ad1a63de53bb314187869136753e6953b230
Alex <liamesskela@gmail.com>
2021-08-18 00:51:50 +0300

    commit

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
-install: clean
-	./gradlew install
+install:
+	./gradlew clean install
 
 run-dist:
 	./build/install/app/bin/app
 
 check-updates:
 	./gradlew dependencyUpdates
+
+make lint:
+	./gradlew checkstyleMain
+
+build:
+	./gradlew clean build
--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void hello(){
+    public static void hello() {
         Scanner scanName = new Scanner(System.in);
         System.out.println("May I have your name?");
         String name = scanName.nextLine();
