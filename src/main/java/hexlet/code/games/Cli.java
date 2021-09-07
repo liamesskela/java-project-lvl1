c2a9dc7a1a482eb20f5842243c91a7d18d68850b
Alex <liamesskela@gmail.com>
2021-09-07 19:32:17 +0300

    added Cli

--- src/main/java/hexlet/code/games/Cli.java
+++ src/main/java/hexlet/code/games/Cli.java
@@ -1,11 +1,11 @@
 package hexlet.code.games;
 
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class Cli {
-//    public static String hello() {
-//        Scanner scanName = new Scanner(System.in);
-//        System.out.print("May I have your name? ");
-//        return scanName.nextLine();
-//    }
+    public static String hello() {
+        Scanner scanName = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        return scanName.nextLine();
+    }
 }
