8fb5cb7064842c95cfb95abb0e81701e4025ddd2
liamesskela <liamesskela@gmail.com>
2021-11-25 01:25:14 +0200

    Even

--- src/main/java/hexlet/code/games/Even.java
+++ src/main/java/hexlet/code/games/Even.java
@@ -13,28 +13,35 @@ public class Even {
         final int count = 3;
         final int someInt = 1000;
         int k = 0;
+        int flag = 0;
+
         for (int i = 0; i < count; i++) {
             int rnd = (int) (Math.random() * someInt);
             System.out.println("Question: " + rnd);
             System.out.print("Your answer: ");
             String name = scanRnd.nextLine();
             int numRnd = rnd % 2;
-            if ((name.equals("yes") && numRnd == 0) || (name.equals("no") && numRnd != 0)) {
+
+            if (numRnd == 0) {
+                flag = 1;
+            }
+            if (numRnd != 0) {
+                flag = 0;
+            }
+
+            if (name.equals("yes") && numRnd == 0) {
+                System.out.println("Correct!");
+                k++;
+            }
+            if (name.equals("no") && numRnd != 0) {
                 System.out.println("Correct!");
                 k++;
             }
 
             if (!name.equals("yes") && numRnd == 0  || !name.equals("no") && numRnd != 0) {
-                if (name.equals("yes")) {
-                    System.out.println("'" + name + "' is wrong answer ;(. Correct answer was 'no'.\n"
+                    System.out.println("'" + name + "' is wrong answer ;(. Correct answer was " + "'" +(flag == 1 ? "yes" : "no")+ "'" + ".\n"
                             + "Let's try again, " + returnName + "!");
                     break;
-                }
-                if (name.equals("no")) {
-                    System.out.println("'" + name + "' is wrong answer ;(. Correct answer was 'yes'.\n"
-                            + "Let's try again, " + returnName + "!");
-                    break;
-                }
             }
         }
         if (k == count) {
